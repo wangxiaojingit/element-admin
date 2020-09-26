4582df57ad7ffb954f8815faedba52eb6a9b2e8e
wangxiaojingit <547154109@qq.com>
2020-09-26 20:03:46 +0800

    aa

---
+++ src/components/Echarts.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="echartsBox">
+    <div class="main" ref="echart">哈哈哈</div>
+  </div>
+</template>
+
+<script>
+import echarts from "echarts";
+
+
+export default {
+  props:["orderData"],
+  mounted() {
+   
+      this.fn();
+   
+   
+  },
+  data() {
+    return {
+      option: {
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"],
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    fn(){
+    console.log(55);
+    console.log(this.orderData);
+    console.log(55); 
+     var ss= echarts.init(this.$refs.echart);
+     // ss.setOption(this.orderData);
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.echartsBox {
+  height: 100%;
+  .main {
+    height: 100%;
+  }
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -46,22 +46,67 @@
             </div>
           </div>
         </div>
-        <div class="center"></div>
-        <div class="bottom"></div>
+        <div class="center">
+          <echarts
+            v-if="echartData.order.series.length"
+            :orderData="echartData.order"
+            :name="message"
+          ></echarts>
+        </div>
+        <div class="bottom">
+          <div class="user">
+            <echarts></echarts>
+          </div>
+          <div class="course">
+            <echarts></echarts>
+          </div>
+        </div>
       </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
+import echarts from "../../components/Echarts";
 export default {
   created() {
     this.$http.get("/home/getData").then((res) => {
-      this.tableData = res.data.data.tableData;
+      var data = res.data.data;
+      this.tableData = data.tableData;
+      console.log(data);
+      //order 的x 轴数据;
+      this.echartData.order.xData = data.orderData.date;
+      // 取出series 中的keys值
+      var keys = Object.keys(data.orderData.data[0]);
+      /* 
+      ES6:5267.53
+      React:2319.4
+      Redis:5957
+      springboot:4894
+      vue:1501.1
+      wechat:748.2
+      */
+      // {
+      //     name: 'Forest',
+      //     type: 'bar',
+      //     barGap: 0,
+      //     label: labelOption,
+      //     data: [320, 332, 301, 334, 390]
+      // },
+      keys.forEach((key) => {
+        this.echartData.order.series.push({
+          name: key == "wechat" ? "微信小程序" : key,
+          data: data.orderData.data.map((item) => {
+            return item[key];
+          }),
+          type: "line",
+        });
+      });
     });
   },
   data() {
     return {
+      message: "hello",
       photo: require("../../assets/photo.jpeg"),
       tableData: [],
       tableLable: {
@@ -108,13 +153,49 @@ export default {
           color: "#5ab1ef",
         },
       ],
+      //echart 图表
+      echartData: {
+        order: {
+          xData: [],
+          series: [],
+        },
+        user: {
+          xData: [],
+          series: [],
+        },
+        video: {
+          series: [],
+        },
+      },
+      option: {
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"],
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      },
     };
+  },
+  components: {
+    echarts,
   },
 };
 </script>
 
 <style lang="less" scoped>
-// @import "../../assets/css/home.less";
 .home {
   display: flex;
   .left {
@@ -161,21 +242,21 @@ export default {
       flex-direction: column;
       .top {
         height: 300px;
-        flex: 5;
+        flex: 6;
         display: flex;
         flex-wrap: wrap;
         align-content: space-between;
         .item {
           width: 33.33%;
           height: 80px;
-         
-         padding-right:10px;
-         box-sizing: border-box;
-          
-          .bd{
-           background: #fff;
-           width:100%;
-           height:100%;
+
+          padding-right: 10px;
+          box-sizing: border-box;
+
+          .bd {
+            background: #fff;
+            width: 100%;
+            height: 100%;
             display: flex;
             align-items: center;
           }
@@ -198,12 +279,22 @@ export default {
       }
       .center {
         margin: 30px 0;
-        background: blue;
-        flex: 10;
+        background: #fff;
+        flex: 8;
       }
       .bottom {
-        flex: 5;
-        background: blue;
+        flex: 10;
+        display: flex;
+        .user {
+          flex: 1;
+          margin-right: 20px;
+          box-sizing: border-box;
+          background: #fff;
+        }
+        .course {
+          flex: 1;
+          background: #fff;
+        }
       }
     }
   }
